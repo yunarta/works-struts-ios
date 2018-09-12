d7905875ff06b6c8f4a429b639b6af54b571b5fb
Yunarta Kartawahyudi <yunarta.kartawahyudi@gmail.com>
2018-09-13 01:15:42 +0800

    Initial commit

---
+++ Struts-Example/Struts-Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Struts-Example
+//
+//  Created by Yunarta on 13/9/18.
+//  Copyright © 2018 mobilesolution works. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Struts/Struts.h
@@ -0,0 +1,19 @@
+//
+//  Struts.h
+//  Struts
+//
+//  Created by Yunarta on 13/9/18.
+//  Copyright © 2018 mobilesolution works. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Struts.
+FOUNDATION_EXPORT double StrutsVersionNumber;
+
+//! Project version string for Struts.
+FOUNDATION_EXPORT const unsigned char StrutsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Struts/PublicHeader.h>
+
+
